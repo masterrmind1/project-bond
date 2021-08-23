2c8fde0288387120b5c186b773ec1e97e9fa2010
Paurnima <paurnima2007@gmail.com>
2021-08-24 00:21:30 +0530

    third commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase:{
-    apiKey: "AIzaSyBVoAJU9FhIpJZmyvSEfuTOIiOnWUU_WLc",
-    authDomain: "project-bond-e6798.firebaseapp.com",
-    projectId: "project-bond-e6798",
-    storageBucket: "project-bond-e6798.appspot.com",
-    messagingSenderId: "703514112234",
-    appId: "1:703514112234:web:b9b9f096e8731f1c06e793",
-    measurementId: "G-NPG6SV3G5P",
-    databaseURL: "https://project-bond-e6798-default-rtdb.asia-southeast1.firebasedatabase.app"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    databaseURL: ""
     }
 };
 
